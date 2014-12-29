8160d94d03c3bcf9fb0963e63c1d7f6cad322043
sdanailova <sdanailova@infragistics.com>
2014-12-29 13:29:35 +0200

    Initial commit 2

---
+++ Presentation/Nop.Web/Models/Common/HeaderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nop.Web.Framework.Mvc;
+
+namespace Nop.Web.Models.Common
+{
+	public partial class HeaderModel : BaseNopModel
+	{
+		public string FacebookLink { get; set; }
+		public string TwitterLink { get; set; }
+		public string YoutubeLink { get; set; }
+		public string GooglePlusLink { get; set; }
+	}
+}
